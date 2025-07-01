792b2062148eb134ba2443b3e1b459b4be572991
Trần Hải Đông <Tranhaidongpk@gmail.com>
2025-07-01 18:16:44 +0700

    add script towercrossbow

---
+++ Assets/Scripts/Tower_CrossBow.cs
@@ -0,0 +1,34 @@
+using UnityEngine;
+
+public class Tower_CrossBow : Tower
+{
+    private CrossBow_Visual visual;
+
+    [Header("Crossbow Details")]
+    [SerializeField] protected Transform gunPoint;
+
+    protected override void Awake()
+    {
+        base.Awake();
+
+        visual = GetComponent<CrossBow_Visual>();
+    }
+
+    protected override void Attack()
+    {
+        if (currentEnemy == null || gunPoint == null)
+        {
+            return; // Không có kẻ thù hoặc gunPoint chưa được gán
+        }
+
+        Vector3 directionToEnemy = DirectionToEnemyFrom(gunPoint);
+
+        if (Physics.Raycast(gunPoint.position, directionToEnemy, out RaycastHit hitInfo, Mathf.Infinity))
+        {
+            towerHead.forward = directionToEnemy; // Quay đầu tháp về phía kẻ thù
+            Debug.DrawLine(gunPoint.position, hitInfo.point);
+
+            visual.PlayAttackVFX(gunPoint.position, hitInfo.point);
+        }
+    }
+}
