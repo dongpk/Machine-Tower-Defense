2db073323b78cf0d9e6b34a21d5e8752ef909019
Trần Hải Đông <Tranhaidongpk@gmail.com>
2025-07-03 16:06:04 +0700

    Update Enemy.cs

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -2,30 +2,54 @@ using System;
 using UnityEngine;
 using UnityEngine.AI;
 
-public class Enemy : MonoBehaviour
+public class Enemy : MonoBehaviour,IDamagable
 {
     private NavMeshAgent agent;
+    public int healthPoints = 4;
     
+    [Header("Movement Settings")]
     [SerializeField] private float turnSpeed = 10f;
     [SerializeField] private float stoppingDistance = 0.5f;
     [SerializeField] private Transform[] Waypoint;
 
+    [Space]
+    private float totalDistance;
+   
+
     private int waypointIndex;
+
 
     void Awake()
     {
         agent = GetComponent<NavMeshAgent>();
-        // agent.updatePosition = false; // Disable automatic position updates
+        agent.updateRotation = false; // Disable automatic position updates
         agent.avoidancePriority = Mathf.RoundToInt (agent.speed * 10);
     }
 
     void Start()
     {
+
         // agent.SetDestination(Waypoint.position);
-        Waypoint = FindFirstObjectByType<WayPontManager>().GetWaypoints(); 
+        Waypoint = FindFirstObjectByType<WayPontManager>().GetWaypoints();
+        CollectTotalDistance();
+
     }
+    public float DistanceToFinishLine()=>totalDistance + agent.remainingDistance;
+    private void CollectTotalDistance()
+    {
+        for (int i = 0; i < Waypoint.Length - 1; i++)
+        {
+            float Distance = Vector3.Distance(Waypoint[i].position, Waypoint[i + 1].position);
+
+            totalDistance += Distance;
+        }
+    }
+
+    
+
     void Update()
     {
+
         FaceTarget(agent.steeringTarget);
         if (agent.remainingDistance < 0.5f)
         {
@@ -52,7 +76,24 @@ public class Enemy : MonoBehaviour
             return transform.position; // Return to the start position if all waypoints are visited
         }
         Vector3 targetPoint = Waypoint[waypointIndex].position;
+
+        if (waypointIndex > 0)
+        {
+            float Distance = Vector3.Distance(Waypoint[waypointIndex].position, Waypoint[waypointIndex - 1].position);
+            totalDistance -= Distance; 
+        }
+
+
         waypointIndex = waypointIndex + 1;
         return targetPoint;
+    }
+
+    public void TakeDamage(int damage)
+    {
+        healthPoints = healthPoints - damage;
+        if (healthPoints <= 0)
+        {
+            Destroy(gameObject);
+        }
     }
 }
