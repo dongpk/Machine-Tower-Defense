eda98cd71e07b2b61f85e60d6f70fe265ca7ccd6
Trần Hải Đông <Tranhaidongpk@gmail.com>
2025-08-06 20:32:25 +0700

    add gamemanager

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,33 @@
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private int currency;
+
+    [SerializeField] private int maxHP;
+    [SerializeField] private int currentHP;
+
+    private UI_InGame inGameUI;
+
+
+    private void Awake()
+    {
+        inGameUI = FindFirstObjectByType<UI_InGame>();
+    }
+    private void Start()
+    {
+        currentHP= maxHP;
+        inGameUI.UpdateHealPointUI(currentHP, maxHP);
+    }
+
+    public void UpdateHP(int value)
+    {
+        currentHP+= value;
+         inGameUI.UpdateHealPointUI(currentHP, maxHP);
+    }
+    public void UpdateCurrency(int value)
+    {
+        currency += value;
+        inGameUI.UpdateCurrencyUI(currency);
+    }
+}
