4f82222b8abff74cfed8b12e44c3125bedae2f9c
Trần Hải Đông <Tranhaidongpk@gmail.com>
2025-08-10 21:22:19 +0700

    Add UI_Pause script for pause menu functionality
    Introduces the UI_Pause MonoBehaviour to manage the pause menu in Unity. Handles pausing and resuming game time, switching between pause UI elements, and returning to the in-game UI when Escape is pressed.

---
+++ Assets/Scripts/UI/UI_Pause.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+
+public class UI_Pause : MonoBehaviour
+{
+    private UI ui;
+    private UI_InGame inGameUI;
+
+    [SerializeField] private GameObject[] pauseUiElements;
+
+    private void Awake()
+    {
+        ui = GetComponentInParent<UI>();
+        inGameUI = GetComponentInChildren<UI_InGame>();
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            ui.SwitchTo(inGameUI.gameObject);
+        }
+    }
+
+    public void SwitchPauseUiElements(GameObject uiElementsToEnable)
+    {
+        foreach (GameObject obj in pauseUiElements)
+        {
+            obj.SetActive(false);
+        }
+        uiElementsToEnable.SetActive(true);
+    }
+
+    private void OnEnable()
+    {
+        Time.timeScale = 0f; // Dừng thời gian khi UI Pause được bật
+    }
+    private void OnDisable()
+    {
+        Time.timeScale = 1f; // Tiếp tục thời gian khi UI Pause bị tắt
+    }
+}
